24d9cc269aaa722d4d8ecc8d030244aea5a6e385
ViswanathVenkatesan <viswanathvenkatesan1328@gmail.com>
2025-07-06 22:42:20 +0530

    config a maven

---
+++ week3/configuring a maven/Configuring_a_Maven/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.library.repository;
+
+public class BookRepository {
+    public void fetchBook() {
+        System.out.println("Fetching book data from repository...");
+    }
+}
+
+
---
+++ week3/configuring a maven/Configuring_a_Maven/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,19 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+    private BookRepository bookRepository;
+
+    // Setter method for DI
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void display() {
+        System.out.println("BookService display method called!");
+        bookRepository.fetchBook();
+    }
+}
+
+
